5c73fca7b3a24176c93af109b18b88b81dfd2133
RawnieCole <65691699+RawnieCole@users.noreply.github.com>
2020-07-20 13:27:47 -0500

    musixmatch api key

--- Assets/musixmatch.js
+++ Assets/musixmatch.js
@@ -1,2 +1,14 @@
-var baseRoute = 'https://api.musixmatch.com/ws/1.1/?apikey=4ea638d2b2686b49ed84acc17be2985e';
-console.log(baseRoute)
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+var baseRoute = 'https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=15953433&apikey=4ea638d2b2686b49ed84acc17be2985e';
+console.log( baseRoute )
+
+$.get( proxyurl + baseRoute )
+    .then( function ( response ) {
+        response = JSON.parse( response )
+        var lyrics = response.message.body.lyrics.lyrics_body
+        console.log( lyrics )
+        $( "#lyrics" ).text( lyrics )
+    } )
+    .catch( function ( error ) {
+        console.log( error )
+    } )

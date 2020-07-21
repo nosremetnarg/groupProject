9468651b6808c7806fa6ff14c3bf0c6af5272a53
Kristin Earnest <kearnest10@gmail.com>
2020-07-21 15:24:35 -0500

    youtube api js updated

--- script.js
+++ script.js
@@ -1,99 +1,67 @@
-// let button = document.querySelector("#search")
-// let output = document.querySelector("#output")
-// let input = document.querySelector("#artist")
+let button = document.getElementById("search")
+let output = document.querySelector("#output")
+let input = document.querySelector("#artist")
 
 
-// button.addEventListener('click', (e) => {
-//     console.log(input.value)
-//     getDataFromYoutube(input.value)
-// })
+button.addEventListener('click', getDataFromYoutube)
+  
 
-// function getDataFromYoutube(artist) {
-//     var url=`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${artist}&key=AIzaSyBQ1GFSeS6gr-Q4QZgRIdpLik6Ijpbv8mc`
-//     console.log(url);
+function getDataFromYoutube(event) {
+    event.preventDefault()
+    let artist = input
+    var url=`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${artist}&key=AIzaSyBQ1GFSeS6gr-Q4QZgRIdpLik6Ijpbv8mc`
+    console.log(url);
+
+    function callYoutube(myExpression) {
+        var queryURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + myExpression + '&safeSearch=strict&key=AIzaSyBQ1GFSeS6gr-Q4QZgRIdpLik6Ijpbv8mc&type=video';
+
+        console.log(myExpression)
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+
+        }).then(function (response) {
+            console.log(response);
+            var results = response.items["0"].id.videoId;
+
+            console.log("this is the variable results:" + results)
 
 
-//     fetch(url)
-//     .then(data => data.json())
-//     .then(data => console.log(data))
-// }
-
-gapi.load("client", loadClient);
- 
-function loadClient() {
-    gapi.client.setApiKey("AIzaSyBQ1GFSeS6gr-Q4QZgRIdpLik6Ijpbv8mc");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-                function(err) { console.error("Error loading GAPI client for API", err); });
-}
-
-
-const ytForm = document.getElementById('yt-form');
-const keywordInput = document.getElementById('keyword-input');
-const maxresultInput = document.getElementById('maxresult-input');
-const orderInput = document.getElementById('order-input');
-const videoList = document.getElementById('videoListContainer');
-var pageToken = '';
- 
-ytForm.addEventListener('submit', e => {
-    e.preventDefault();
-    execute();
-});
- 
-function paginate(e, obj) {
-    e.preventDefault();
-    pageToken = obj.getAttribute('data-id');
-    execute();
-}
- 
-
-function execute() {
-    const searchString = keywordInput.value;
-    const maxresult = maxresultInput.value;
-    const orderby = orderInput.value;
- 
-    
-    var arr_search = {
-        "part": 'snippet',
-        "type": 'video',
-        "order": orderby,
-        "maxResults": maxresult,
-        "q": searchString
-    };
- 
-    if (pageToken != '') {
-        arr_search.pageToken = pageToken;
+            $("#youtubeDiv").html('<iframe id="youtubeFrame" style="background-size: cover" src="https://www.youtube.com/embed/' + results + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+        })
     }
- 
-    return gapi.client.youtube.search.list(arr_search)
-    .then(function(response) {
-        const listItems = response.result.items;
+}
 
-        console.log(arr_search);
-        console.log(response.result.items);
-        if (listItems) {
-            let output = '<h4>Videos</h4><ul>';
- 
-            listItems.forEach(item => {
-                const videoId = item.id.videoId;
-                const videoTitle = item.snippet.title;
-                output += `
-                    <li><a data-fancybox href="https://www.youtube.com/watch?v=${videoId}"><img src="http://i3.ytimg.com/vi/${videoId}/hqdefault.jpg" /></a><p>${videoTitle}</p></li>
-                `;
-            });
-            output += '</ul>';
- 
-            if (response.result.prevPageToken) {
-                output += `<br><a class="paginate" href="#" data-id="${response.result.prevPageToken}" onclick="paginate(event, this)">Prev</a>`;
-            }
- 
-            if (response.result.nextPageToken) {
-                output += `<a href="#" class="paginate" data-id="${response.result.nextPageToken}" onclick="paginate(event, this)">Next</a>`;
-            }
- 
+$(document).ready(function () {
+    $('#search-term').submit(function (event) {
+        event.preventDefault();
+        var searchTerm = $('#query').val();
+        getRequest(searchTerm);
+    });
+});
+
+function getRequest(searchTerm) {
+    var url = 'https://www.googleapis.com/youtube/v3/search';
+    var params = {
+        part: 'snippet',
+        key: 'XXXXXX',
+        q: searchTerm
+    };
+  
+    $.getJSON(url, params, showResults);
+}
+
+function showResults(results) {
+    var html = "";
+    var entries = results.items;
     
-            videoList.innerHTML = output;
-        }
-    },
-    function(err) { console.error("Execute error", err); });
+    $.each(entries, function (index, value) {
+        var title = value.snippet.title;
+        var thumbnail = value.snippet.thumbnails.default.url;
+        html += '<p>' + title + '</p>';
+        html += '<img src="' + thumbnail + '">';
+    }); 
+    
+    $('#search-results').html(html);
 }
